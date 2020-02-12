2140bc9d4e8c3868e46f8747a75b4656955caea2
David Meshnick <dcmesh@Davids-MacBook-Pro.local>
2020-02-11 23:06:50 -0500

    simplified equality lines

--- InterpreterBoolean.rkt
+++ InterpreterBoolean.rkt
@@ -12,14 +12,10 @@
       ((or (eq? #t expression) (eq? #f expression)))
       ((eq? '== (operator expression)) (eq? (value (leftoperand expression) state) (value (rightoperand expression) state)))
       ((eq? '!= (operator expression)) (not (eq? (value (leftoperand expression) state) (value (rightoperand expression) state))))
-      ((eq? '<= (operator expression)) (or
-                                            (eq? (value (leftoperand expression) state) (value (rightoperand expression) state))
-                                            (< (value (leftoperand expression) state) (value (rightoperand expression) state))))
-      ((eq? '>= (operator expression)) (or
-                                            (eq? (value (leftoperand expression) state) (value (rightoperand expression) state))
-                                            (> (value (leftoperand expression) state) (value (rightoperand expression) state))))
       ((eq? '> (operator expression))  (> (value (leftoperand expression) state) (value (rightoperand expression) state)))
       ((eq? '< (operator expression))  (< (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '<= (operator expression))  (<= (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '>= (operator expression))  (>= (value (leftoperand expression) state) (value (rightoperand expression) state)))
       ((eq? '&& (operator expression)) (and (eq? (Mbool (leftoperand expression) state) #t) (eq? (Mbool (rightoperand expression) state) #t)))
       ((eq? '|| (operator expression)) (or (eq? (Mbool (leftoperand expression) state) #t) (eq? (Mbool (rightoperand expression) state) #t)))
       (else (error 'badop "The operator is not known, or type mismatch")))))

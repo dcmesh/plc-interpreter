21ecb095a4f67bdcce82ff4dc351edb413eabfde
David Meshnick <dcmesh@Davids-MacBook-Pro.local>
2020-02-13 14:21:18 -0500

    moved boolean function to InterpreterValue.rkt to prevent circular dependence, deleted redundant file

--- InterpreterValue.rkt
+++ InterpreterValue.rkt
@@ -33,3 +33,22 @@
       ((eq? '/ (operator expression)) (quotient (value (leftoperand expression) state) (value (rightoperand expression) state)))
       ((eq? '% (operator expression)) (modulo (value (leftoperand expression) state) (value (rightoperand expression) state)))
       (else (error 'bad_operation "The operator is not known")))))
+
+; Returns the boolean value as determined by the operator
+(define expressionBoolean
+  (lambda (expression state)
+    (cond
+      ((null? expression) (error 'parser "parser should have caught this"))
+      ((or (eq? #t expression) (eq? #f expression)))
+      ((eq? '== (operator expression)) (eq? (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '!= (operator expression)) (not (eq? (value (leftoperand expression) state) (value (rightoperand expression) state))))
+      ((eq? '> (operator expression))  (> (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '< (operator expression))  (< (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '<= (operator expression))  (<= (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '>= (operator expression))  (>= (value (leftoperand expression) state) (value (rightoperand expression) state)))
+      ((eq? '&& (operator expression)) (and (eq? (expressionBoolean (leftoperand expression) state) #t) (eq? (expressionBoolean (rightoperand expression) state) #t)))
+      ((eq? '|| (operator expression)) (or (eq? (expressionBoolean (leftoperand expression) state) #t) (eq? (expressionBoolean (rightoperand expression) state) #t)))
+      ((eq? '! (operator expression)) (not (expressionBoolean (leftoperand expression) state)))
+      (else (error 'badop "The operator is not known, or type mismatch")))))
+       
+

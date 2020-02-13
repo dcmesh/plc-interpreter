380f4af84ad82a252a05f580a00bf1a90df0df73
Austin Keppers <akeppers21@gmail.com>
2020-02-13 00:56:55 -0500

    #t and #f convert to true and false on output

--- InterpreterMain.rkt
+++ InterpreterMain.rkt
@@ -6,6 +6,13 @@
 (define interpret
   (lambda (program state)
     (cond
-      ((isDeclared 'return (variablenames state)) (value 'return state))
+      ((isDeclared 'return (variablenames state)) (sanitizeReturn (value 'return state)))
       ((null? program) (error 'no_return "program end reached without a return"))
       (else (interpret (cdr program) (updateState (car program) state))))))
+
+(define sanitizeReturn
+  (lambda (expression)
+    (cond
+      ((eq? expression #t) 'true)
+      ((eq? expression #f) 'false)
+      (else expression))))

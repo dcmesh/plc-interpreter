d9b907fca9a5e0de1a61e5f2f85c034e882a5d09
Austin Keppers <akeppers21@gmail.com>
2020-04-01 14:08:33 -0400

    Hopefully fixed add params layer

--- InterpreterState.rkt
+++ InterpreterState.rkt
@@ -99,10 +99,10 @@
 (define add-params-layer
   (lambda (formal actual state break continue return throw)
     (cond
-      ((and (null? formal) (null? actual)) (init-layer))
+      ((and (null? formal) (null? actual)) init-layer)
       ((or (null? formal) (null? actual)) (error 'mismatched_Arguments "Incorrect amount of arugments encountered"))
       (else (bind-to-layer (car formal) (box
-                                         (value (car actual) state break continue return throw))
+                                         (value (car actual) state))
                            (add-params-layer (cdr formal)
                                              (cdr actual)
                                              state break continue return throw))))))
